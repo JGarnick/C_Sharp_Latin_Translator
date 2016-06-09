4214cd56d69f78201100be66a457fc8e0759021b
JGarnick <garnick.josh@gmail.com>
2016-06-09 16:11:35 -0700

    Simple app to demonstrate basics
    translates hardcoded words from latin to english

---
+++ Latin Translator/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Latin_Translator
+{
+    partial class latinTranslatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.latinLabel = new System.Windows.Forms.Label();
+            this.sinsiterLabel = new System.Windows.Forms.Label();
+            this.dexterLabel = new System.Windows.Forms.Label();
+            this.mediumLabel = new System.Windows.Forms.Label();
+            this.englishLabel = new System.Windows.Forms.Label();
+            this.translateSinisterButton = new System.Windows.Forms.Button();
+            this.translateDexterButton = new System.Windows.Forms.Button();
+            this.translateMediumButton = new System.Windows.Forms.Button();
+            this.translateSinisterLabel = new System.Windows.Forms.Label();
+            this.translateDexterLabel = new System.Windows.Forms.Label();
+            this.translateMediumLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // latinLabel
+            // 
+            this.latinLabel.AutoSize = true;
+            this.latinLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.latinLabel.Location = new System.Drawing.Point(21, 42);
+            this.latinLabel.Name = "latinLabel";
+            this.latinLabel.Size = new System.Drawing.Size(154, 31);
+            this.latinLabel.TabIndex = 0;
+            this.latinLabel.Text = "Latin Word";
+            // 
+            // sinsiterLabel
+            // 
+            this.sinsiterLabel.AutoSize = true;
+            this.sinsiterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sinsiterLabel.Location = new System.Drawing.Point(56, 126);
+            this.sinsiterLabel.Name = "sinsiterLabel";
+            this.sinsiterLabel.Size = new System.Drawing.Size(71, 24);
+            this.sinsiterLabel.TabIndex = 2;
+            this.sinsiterLabel.Text = "Sinister";
+            // 
+            // dexterLabel
+            // 
+            this.dexterLabel.AutoSize = true;
+            this.dexterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dexterLabel.Location = new System.Drawing.Point(56, 199);
+            this.dexterLabel.Name = "dexterLabel";
+            this.dexterLabel.Size = new System.Drawing.Size(65, 24);
+            this.dexterLabel.TabIndex = 3;
+            this.dexterLabel.Text = "Dexter";
+            // 
+            // mediumLabel
+            // 
+            this.mediumLabel.AutoSize = true;
+            this.mediumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumLabel.Location = new System.Drawing.Point(56, 276);
+            this.mediumLabel.Name = "mediumLabel";
+            this.mediumLabel.Size = new System.Drawing.Size(79, 24);
+            this.mediumLabel.TabIndex = 4;
+            this.mediumLabel.Text = "Medium";
+            // 
+            // englishLabel
+            // 
+            this.englishLabel.AutoSize = true;
+            this.englishLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.englishLabel.Location = new System.Drawing.Point(304, 42);
+            this.englishLabel.Name = "englishLabel";
+            this.englishLabel.Size = new System.Drawing.Size(186, 31);
+            this.englishLabel.TabIndex = 5;
+            this.englishLabel.Text = "English Word";
+            // 
+            // translateSinisterButton
+            // 
+            this.translateSinisterButton.Location = new System.Drawing.Point(196, 129);
+            this.translateSinisterButton.Name = "translateSinisterButton";
+            this.translateSinisterButton.Size = new System.Drawing.Size(75, 23);
+            this.translateSinisterButton.TabIndex = 6;
+            this.translateSinisterButton.Text = "translate!";
+            this.translateSinisterButton.UseVisualStyleBackColor = true;
+            this.translateSinisterButton.Click += new System.EventHandler(this.translateSinisterButton_Click);
+            // 
+            // translateDexterButton
+            // 
+            this.translateDexterButton.Location = new System.Drawing.Point(196, 202);
+            this.translateDexterButton.Name = "translateDexterButton";
+            this.translateDexterButton.Size = new System.Drawing.Size(75, 23);
+            this.translateDexterButton.TabIndex = 7;
+            this.translateDexterButton.Text = "translate!";
+            this.translateDexterButton.UseVisualStyleBackColor = true;
+            this.translateDexterButton.Click += new System.EventHandler(this.translateDexterButton_Click);
+            // 
+            // translateMediumButton
+            // 
+            this.translateMediumButton.Location = new System.Drawing.Point(196, 279);
+            this.translateMediumButton.Name = "translateMediumButton";
+            this.translateMediumButton.Size = new System.Drawing.Size(75, 23);
+            this.translateMediumButton.TabIndex = 8;
+            this.translateMediumButton.Text = "translate!";
+            this.translateMediumButton.UseVisualStyleBackColor = true;
+            this.translateMediumButton.Click += new System.EventHandler(this.translateMediumButton_Click);
+            // 
+            // translateSinisterLabel
+            // 
+            this.translateSinisterLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.translateSinisterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateSinisterLabel.Location = new System.Drawing.Point(324, 120);
+            this.translateSinisterLabel.Name = "translateSinisterLabel";
+            this.translateSinisterLabel.Size = new System.Drawing.Size(138, 36);
+            this.translateSinisterLabel.TabIndex = 9;
+            this.translateSinisterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // translateDexterLabel
+            // 
+            this.translateDexterLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.translateDexterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateDexterLabel.Location = new System.Drawing.Point(324, 193);
+            this.translateDexterLabel.Name = "translateDexterLabel";
+            this.translateDexterLabel.Size = new System.Drawing.Size(138, 37);
+            this.translateDexterLabel.TabIndex = 10;
+            this.translateDexterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // translateMediumLabel
+            // 
+            this.translateMediumLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.translateMediumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateMediumLabel.Location = new System.Drawing.Point(324, 269);
+            this.translateMediumLabel.Name = "translateMediumLabel";
+            this.translateMediumLabel.Size = new System.Drawing.Size(138, 39);
+            this.translateMediumLabel.TabIndex = 11;
+            this.translateMediumLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(224, 366);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(87, 29);
+            this.exitButton.TabIndex = 12;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // latinTranslatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 424);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.translateMediumLabel);
+            this.Controls.Add(this.translateDexterLabel);
+            this.Controls.Add(this.translateSinisterLabel);
+            this.Controls.Add(this.translateMediumButton);
+            this.Controls.Add(this.translateDexterButton);
+            this.Controls.Add(this.translateSinisterButton);
+            this.Controls.Add(this.englishLabel);
+            this.Controls.Add(this.mediumLabel);
+            this.Controls.Add(this.dexterLabel);
+            this.Controls.Add(this.sinsiterLabel);
+            this.Controls.Add(this.latinLabel);
+            this.Name = "latinTranslatorForm";
+            this.Text = "Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label latinLabel;
+        private System.Windows.Forms.Label sinsiterLabel;
+        private System.Windows.Forms.Label dexterLabel;
+        private System.Windows.Forms.Label mediumLabel;
+        private System.Windows.Forms.Label englishLabel;
+        private System.Windows.Forms.Button translateSinisterButton;
+        private System.Windows.Forms.Button translateDexterButton;
+        private System.Windows.Forms.Button translateMediumButton;
+        private System.Windows.Forms.Label translateSinisterLabel;
+        private System.Windows.Forms.Label translateDexterLabel;
+        private System.Windows.Forms.Label translateMediumLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Latin Translator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latin_Translator
+{
+    public partial class latinTranslatorForm : Form
+    {
+        public latinTranslatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void translateSinisterButton_Click(object sender, EventArgs e)
+        {
+            translateSinisterLabel.Text = "left";
+            translateDexterLabel.Text = "";
+            translateMediumLabel.Text = "";
+        }
+
+        private void translateDexterButton_Click(object sender, EventArgs e)
+        {
+            translateSinisterLabel.Text = "";
+            translateDexterLabel.Text = "right";
+            translateMediumLabel.Text = "";
+        }
+
+        private void translateMediumButton_Click(object sender, EventArgs e)
+        {
+            translateSinisterLabel.Text = "";
+            translateDexterLabel.Text = "";
+            translateMediumLabel.Text = "center";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
